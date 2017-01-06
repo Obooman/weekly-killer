7e2be690fefac649d1e57380635886432c98959c
Oboo Chin <cheng.12@live.cn>
2017-01-06 13:33:54 +0800

    Update usage,

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ var gmail = {};
 gmail.ready = function(func){
 	gmail._ready = func;
 }
+
+gmail.getCalendars =  function(start,end,callback){
+	var request = gapi.client.calendar.events.list({
+		calendarId:'oboochin@gmail.com',
+		timeMax:end.toJSON(),
+		timeMin:start.toJSON()
+	})
+
+	request.execute(function(res){
+		var tasks = res.items;
+
+		callback && callback(tasks);
+	})
+}
